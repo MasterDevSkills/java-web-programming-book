1e3c475b524afc0df7d1de88711c670faf3104a8
Bazlur Rahman <anmbrr.bit0112@gmail.com>
2020-01-01 05:55:47 -0500

    Add eshoppers project structure

---
+++ chapter-03/src/main/java/com/bazlur/eshoppers/web/HomeServlet.java
@@ -0,0 +1,22 @@
+package com.bazlur.eshoppers.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println(
+                "<html>" +
+                        "<body>" +
+                            "<h1> Hello, world! </h1>" +
+                        "</body>" +
+                "</html>"
+        );
+    }
+}
