87e85f296ef6138b2c2c85d0392a99acfcec0027
Bazlur Rahman <anmbrr.bit0112@gmail.com>
2020-01-16 19:30:50 -0500

    Fix CartRepositoryImpl and LoginServlet in chapter 5

--- chapter-05/src/main/java/com/bazlur/eshoppers/repository/CartRepositoryImpl.java
+++ chapter-05/src/main/java/com/bazlur/eshoppers/repository/CartRepositoryImpl.java
@@ -7,25 +7,52 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import java.util.*;
+
 public class CartRepositoryImpl implements CartRepository {
-	private static final Map<User, Cart> CARTS = new HashMap<>();
+	private static final Map<User, LinkedHashSet<Cart>>
+					CARTS = new HashMap<>();
 
 	@Override
 	public Optional<Cart> findByUser(User currentUser) {
-		Cart cart = CARTS.get(currentUser);
 
-		return Optional.ofNullable(cart);
+		var carts = CARTS.get(currentUser);
+		if (!carts.isEmpty()) {
+
+			Cart cart = (Cart) carts.toArray()[carts.size() - 1];
+			return Optional.of(cart);
+		}
+
+		return Optional.empty();
 	}
 
 	@Override
 	public Cart save(Cart cart) {
+		CARTS.computeIfPresent(cart.getUser(),
+						(user, carts) -> {
+							carts.add(cart);
+							return carts;
+						});
 
-		return CARTS.put(cart.getUser(), cart);
+		CARTS.computeIfAbsent(cart.getUser(),
+						user -> {
+							var carts = new LinkedHashSet<Cart>();
+							carts.add(cart);
+							return carts;
+						});
+
+		return cart;
 	}
 
 	@Override
 	public Cart update(Cart cart) {
+		CARTS.computeIfAbsent(cart.getUser(),
+						user -> {
+							var carts = new LinkedHashSet<Cart>();
+							carts.add(cart);
+							return carts;
+						});
 
-		return CARTS.put(cart.getUser(), cart);
+		return cart;
 	}
 }
