15a7cef6989dd960242a4f3d96080cbe6f2a8322
Bazlur Rahman <anmbrr.bit0112@gmail.com>
2020-01-29 07:34:34 -0500

    Add database configuration

---
+++ chapter-08/src/main/java/com/bazlur/eshoppers/jdbc/ConnectionPool.java
@@ -0,0 +1,34 @@
+package com.bazlur.eshoppers.jdbc;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.util.ResourceBundle;
+
+public final class ConnectionPool {
+
+	private static final ConnectionPool INSTANCE
+					= new ConnectionPool();
+
+	private ConnectionPool() {
+	}
+
+	public static ConnectionPool getInstance() {
+		return INSTANCE;
+	}
+
+	public DataSource getDataSource() {
+		var dbProp = ResourceBundle.getBundle("db");
+
+		var config = new HikariConfig();
+		config.setJdbcUrl(dbProp.getString("db.url"));
+		config.setUsername(dbProp.getString("db.user"));
+		config.setPassword(dbProp.getString("db.password"));
+		var maxPoolSize
+							= dbProp.getString("db.max.connections");
+		config.setMaximumPoolSize(Integer.parseInt(maxPoolSize));
+
+		return new HikariDataSource(config);
+	}
+}
